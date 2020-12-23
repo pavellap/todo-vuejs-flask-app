38de791f4d2282ebb057322006ea72479bc13634
Pavel Lapshin <52813904+pavellap@users.noreply.github.com>
2020-12-24 01:54:20 +0300

    Delete db.py

--- server/db.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import pymongo
-CONNECT_PARAMS = "mongodb+srv://pavellap:A9MphHHvmrsJrJ2i@firstcluster.cxefr.mongodb.net/<dbname>?retryWrites=true&w" \
-                 "=majority "
-
-client = pymongo.MongoClient(CONNECT_PARAMS)
-db = client.get_database('fullstack-todo')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
-
-
